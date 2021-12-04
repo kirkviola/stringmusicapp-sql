04b5c767b6849b4b1cf61cdd63b7c32c8c82065a
Matthew Kirkendall <kirk.viola@gmail.com>
2021-12-04 13:20:46 -0500

    initial commit

---
+++ init-users.sql
@@ -0,0 +1,3 @@
+INSERT Users (Username, Password, Firstname, Lastname, Email, IsAdmin)
+VALUES  ('KirkAdmin', 'admin', 'Matt', 'Kirkendall', 'kirk.viola@gmail.com', 1),
+		('testone', 'password', 'Test,', 'Dummy', 'dummy@dumbmail.com', 0);
